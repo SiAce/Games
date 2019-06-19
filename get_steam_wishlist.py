ed7527a4003cd292f5648c32e40ced35ef90fa2c
SiAce <siacespark@gmail.com>
2019-06-19 15:20:48 +0800

    Using lxml and XPath to Find the Element

--- get_steam_wishlist.py
+++ get_steam_wishlist.py
@@ -1,5 +1,5 @@
 import urllib.request, urllib.parse, urllib.error
-import lxml
+import lxml.html, lxml.etree
 import ssl
 
 # Ignore SSL certificate errors
@@ -7,17 +7,22 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
+# Compose URL
 steam_url = "https://store.steampowered.com/wishlist/profiles/"
 user_id = input('Enter user id: ')
 wishlist_url = steam_url + user_id
 
+# Get HTML from Steam Web
 response = urllib.request.urlopen(wishlist_url, context=ctx)
-html = response.read().decode()
+html_str = response.read().decode()
 
-htmlparser = lxml.etree.HTMLParser()
-tree = lxml.etree.parse(html, htmlparser)
+# Save HTML file to local (This is added because parsing directly from web didn't work!)
+with open("wishlist.html", "w", encoding="utf-8" ) as html_file:
+    html_file.write(html_str)
 
-xpathselector = "html/body/div[1]/div[7]/div[4]/script/text()"
-data = tree.xpath("/html/body/div[1]/div[7]/div[4]/script")
+# Use lxml and XPath to find the wanted element
+html = lxml.html.parse("wishlist.html")
+xpathselector = "/html/body/div[1]/div[7]/div[4]/script/text()"
+nodes = html.xpath(xpathselector)
 
-print(data)
+print(nodes[0])
