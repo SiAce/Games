86d456e591ee0462a366fa9523d565c978899481
Zete Dai <siacespark@gmail.com>
2019-12-24 11:42:20 -0500

    scrape steam wishlist

--- appid_to_database.py
+++ appid_to_database.py
@@ -2,6 +2,7 @@ import urllib.request, urllib.parse, urllib.error
 import lxml.html, lxml.etree
 import ssl
 import re
+import csv
 
 with open('appid_list.txt', 'r') as appid_file:
     appid_str = appid_file.read()
@@ -15,67 +16,54 @@ ctx.verify_mode = ssl.CERT_NONE
 
 app_prefix = "https://store.steampowered.com/app/"
 
-appid = appid_list[0]
+with open("data_steam.csv", "w", newline="") as data_steam_file:
 
-# Compose URL
-app_url = app_prefix + appid
+    data_steam_writer = csv.writer(data_steam_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-# Get HTML from Steam Web
-response = urllib.request.urlopen(app_url, context=ctx)
-html_str = response.read().decode()
+    data_steam_writer.writerow(["Names", "Reviews", "Release Date", "Genre"])
 
-# Save HTML file to local (Because parsing directly from web didn't work!)
-with open("app.html", "w", encoding="utf-8" ) as html_file:
-    html_file.write(html_str)
+    for appid in appid_list:
 
-# Use lxml and XPath to find the name, reviews, date, genre
-html = lxml.html.parse("app.html")
-name_xpath = '/html/body/div[1]/div[7]/div[4]/div[1]/div[2]/div[2]/div[2]/div/div[3]/text()'
-reviews_xpath = '//*[@id="game_highlights"]/div[1]/div/div[3]/div/div[1]/div[2]/span[3]/text()'
-#reviews_xpath = '//*[@id="game_highlights"]/div[1]/div/div[3]/div/div[2]/div[2]/span[3]/text()'
-release_date_xpath = '//*[@id="game_highlights"]/div[1]/div/div[3]/div/div[2]/div[2]/text()'
-#release_date_xpath = '//*[@id="game_highlights"]/div[1]/div/div[3]/div/div[3]/div[2]/text()'
+        # Compose URL
+        app_url = app_prefix + appid
 
-genre_xpath = '//*[@id="game_highlights"]/div[1]/div/div[4]/div/div[2]/a[1]/text()'
+        # Get HTML from Steam Web
+        response = urllib.request.urlopen(app_url, context=ctx)
+        html_str = response.read().decode()
 
-name = html.xpath(name_xpath)
-reviews = html.xpath(reviews_xpath)
-release_date = html.xpath(release_date_xpath)
-genre = html.xpath(genre_xpath)
+        # Save HTML file to local (Because parsing directly from web didn't work!)
+        with open("app.html", "w", encoding="utf-8" ) as html_file:
+            html_file.write(html_str)
 
-name_str = str(name[0])
-reviews_str = str(reviews[0])
-release_date_str = str(release_date[0])
-genre_str = str(genre[0])
+        # Use lxml and XPath to find the name, reviews, date, genre
+        html = lxml.html.parse("app.html")
+        name_xpath_1 = '/html/body/div[1]/div[7]/div[4]/div[1]/div[2]/div[2]/div[2]/div/div[3]/text()'
+        name_xpath_2 = '/html/body/div[1]/div[7]/div[4]/div[1]/div[2]/div[1]/div[2]/div/div[3]/text()'
+        reviews_xpath_1 = '//*[@id="game_highlights"]/div[1]/div/div[3]/div/div[1]/div[2]/span[3]/text()'
+        reviews_xpath_2 = '//*[@id="game_highlights"]/div[1]/div/div[3]/div/div[2]/div[2]/span[3]/text()'
+        release_date_xpath_1 = '//*[@id="game_highlights"]/div[1]/div/div[3]/div/div[2]/div[2]/text()'
+        release_date_xpath_2 = '//*[@id="game_highlights"]/div[1]/div/div[3]/div/div[3]/div[2]/text()'
+        genre_xpath = '//*[@id="game_highlights"]/div[1]/div/div[4]/div/div[2]/a[1]/text()'
 
-print(name_str)
-print(reviews_str)
-print(release_date_str)
-print(genre_str)
+        name_list_1 = html.xpath(name_xpath_1)
+        name_list_2 = html.xpath(name_xpath_2)
+        reviews_list_1 = html.xpath(reviews_xpath_1)
+        reviews_list_2 = html.xpath(reviews_xpath_2)
+        release_date_list_1 = html.xpath(release_date_xpath_1)
+        release_date_list_2 = html.xpath(release_date_xpath_2)
+        genre_list = html.xpath(genre_xpath)
 
-""" for appid in appid_list:
+        name_1 = str(name_list_1[0]).strip() if name_list_1 else ""
+        name_2 = str(name_list_2[0]).strip() if name_list_2 else ""
+        reviews_1 = str(reviews_list_1[0]).strip()[2:6].strip() if reviews_list_1 else ""
+        reviews_2 = str(reviews_list_2[0]).strip()[2:6].strip() if reviews_list_2 else ""
+        release_date_1 = str(release_date_list_1[0]).strip() if release_date_list_1 else ""
+        release_date_2 = str(release_date_list_2[0]).strip() if release_date_list_2 else ""
+        genre = str((genre_list)[0]).strip() if genre_list else "" 
 
-    # Compose URL
-    app_url = app_prefix + appid
+        name = name_1 if name_1 else name_2
+        reviews = reviews_2 if reviews_2 else reviews_1
+        release_date = release_date_2 if release_date_2 else release_date_1
+        genre = genre if genre else ""
 
-    # Get HTML from Steam Web
-    response = urllib.request.urlopen(app_url, context=ctx)
-    html_str = response.read().decode()
-
-    # Save HTML file to local (Because parsing directly from web didn't work!)
-    with open("app.html", "w", encoding="utf-8" ) as html_file:
-        html_file.write(html_str)
-    
-    # Use lxml and XPath to find the script element which contains wishlist
-    html = lxml.html.parse("app.html")
-    name_xpath = '/html/body/div[1]/div[7]/div[4]/div[1]/div[2]/div[2]/div[2]/div/div[3]/text()'
-    reviews_xpath = '//*[@id="game_highlights"]/div[1]/div/div[3]/div/div[1]/div[2]/span[3]/text()'
-    release_date_xpath = '//*[@id="game_highlights"]/div[1]/div/div[3]/div/div[2]/div[2]/text()'
-    genre_xpath = '//*[@id="game_highlights"]/div[1]/div/div[4]/div/div[2]/a[1]/text()'
-
-    name = html.xpath(name_xpath)
-    reviews = html.xpath(reviews_xpath)
-    release_date = html.xpath(release_date_xpath)
-    genre_xpath = html.xpath(genre_xpath)
-
-    script_str = str(nodes[0]) """
+        data_steam_writer.writerow([name, reviews, release_date, genre])
